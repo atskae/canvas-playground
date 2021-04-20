a9094852bdceb5267dc1ca931ffe9dfde5d058d7
Atsuko Shimizu <atskae@gmail.com>
2021-04-20 07:08:14 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Canvas Tutorial
+Learning about HTML5 Canvas 🎨 because I want to know how drawing apps like [ChickenPaint](https://github.com/thenickdude/chickenpaint) works.
+
+## Resources
+* [Basic usage of Canvas](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Basic_usage)
+* [Learn HTML5 Canvas By Creating A Drawing App (video)](https://www.youtube.com/watch?v=3GqUM4mEYKA)
+
+## Notes
---
+++ src/js/script.js
@@ -0,0 +1,34 @@
+function resizeCanvas() {
+    // Resize the canvas to the same size as
+    // the browser window
+    const canvas = document.getElementById("canvas")
+    canvas.height = Math.floor(window.innerHeight * 0.75)
+    canvas.width = Math.floor(window.innerWidth * 0.75)
+    console.log('Canvas resized to ' + canvas.width + ' by ' + canvas.height)
+}
+
+function draw() {
+    // Constant reference to a value, not a const value!
+    const canvas = document.getElementById("canvas")
+    const msg = document.getElementById("message")
+
+    if (canvas.getContext) {
+        msg.innerHTML = "Canvas is supported! :)"
+        // Resize canvas to browser
+        resizeCanvas()
+
+        // Select the 2D context
+        const ctx = canvas.getContext("2d")
+        console.log(ctx)
+    } else {
+        msg = document.getElementById("message")
+        msg.innerHTML = "Canvas is not supported :("
+    }
+}
+
+/*
+    Event Listeners
+*/
+
+// On browser resize, resize the canvas
+window.addEventListener("resize", resizeCanvas);
